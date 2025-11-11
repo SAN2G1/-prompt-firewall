a9d8dcee96d8e72698c321d3d7f0cffb4802cd42
SAN2G1 <aabbyun@naver.com>
2025-11-11 21:18:46 +0900

    test(stage2): add testing code for stage2 module

--- tester_framework/runners.py
+++ tester_framework/runners.py
@@ -7,6 +7,10 @@ try:
     from stage1_filter import Stage1Filter
 except ImportError:
     print("Warning: 'filter_logic.stage1_filter'를 찾을 수 없습니다.")
+try:
+    from stage2_scorer import Stage2Scorer
+except ImportError:
+    print("Warning: 'filter_logic.stage2_scorer'를 찾을 수 없습니다.")
 
 
 class IFilterRunner(ABC):
@@ -42,6 +46,35 @@ class Stage1LocalRunner(IFilterRunner):
             seed.s1_message = msg
         except Exception as e:
             seed.error = str(e)
-            seed.s1_decision = "ERROR"
+            seed.s1_decision = "s1_ERROR"
+            
+        return seed
+    
+class Stage2LocalRunner(IFilterRunner):
+    """
+    Stage 2 스코어러를 실행하는 실행기
+    """
+    def __init__(self):
+        print("Loading Stage 2 Scorer in runner...")
+        self.scorer_instance = Stage2Scorer()
+        if not self.scorer_instance.models_loaded:
+            print("Warning: Models not loaded in Stage2Scorer.")
+        else : 
+            print("Stage2Scorer models loaded successfully in runner.")    
+    def run(self, seed: Seed) -> Seed:
+        """
+        Stage2Scorer의 predict()를 호출하고 S2 결과를 Seed에 저장합니다.
+        """
+        try:
+            if seed.s1_decision == "ESCALATE":
+                decision, risk_score = self.scorer_instance.predict(seed.data)
+                seed.s2_decision = decision
+                seed.s2_risk_score = risk_score
+            else:
+                seed.s2_decision = "N/A"
+                seed.s2_risk_score = None
+        except Exception as e:
+            seed.error = str(e)
+            seed.s2_decision = "s2_ERROR"
             
         return seed
